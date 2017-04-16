5693eb04ca26eb6b7c47c621292e0959bea6ab0f
RuochenHan <RuochenHan@192.168.1.145>
2017-04-15 23:49:08 -0700

    all method is done before update

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,3 +1,4 @@
+import java.lang.Math; 
 public class Planet {
     // declare instance variables
     public double xxPos; // Its current x position
@@ -8,6 +9,8 @@ public class Planet {
     public String imgFileName; /* The name of an image in the image directory 
                                    that depicts the plant
                                 */
+    //public Planet[] allPlanets; // each element is in array is stand for a planet
+    
     // Planet constructor1
     public Planet (double xP, double yP, double xV, double yV, double m, String img) {
         this.xxPos = xP;
@@ -26,10 +29,53 @@ public class Planet {
         this.yyVel = p.yyVel;
         this.mass = p.mass;
         this.imgFileName = p.imgFileName;
-        
     }
     
+    // Calculate the distance between two planets
+    public double calcDistance (Planet that) {
+        return Math.sqrt((that.xxPos - this.xxPos)*(that.xxPos - this.xxPos) 
+                + (that.yyPos - this.yyPos)*(that.yyPos - this.yyPos));
+    }
     
+    // Calculate the force exert by other planet
+    public double calcForceExertedBy (Planet that) {
+        final double G = 6.67E-11;
+        return G*that.mass*this.mass/(calcDistance(that)*calcDistance(that));
+    }
+    
+    // Calculate the net force in X direction
+    public double calcNetForceExertedByX (Planet[] allPlanets) {
+        double Fx = 0.0;
+        double sum = 0.0;
+        for (int i = 0; i < allPlanets.length; i++) {
+            if (allPlanets[i].xxPos == this.xxPos) {
+                Fx = 0.0;
+            } else {
+                Fx = this.calcForceExertedBy(allPlanets[i])
+                    *(allPlanets[i].xxPos - this.xxPos)
+                    /this.calcDistance(allPlanets[i]);
+            }
+            sum += Fx;
+        }
+        return sum;
+    }
+    
+    // Calculate the net force in Y direction
+    public double calcNetForceExertedByY (Planet[] allPlanets) {
+        double Fy = 0.0;
+        double sum = 0.0;
+        for (int i = 0; i < allPlanets.length; i++) {
+            if (allPlanets[i].yyPos == this.yyPos) {
+                Fy = 0.0;
+            } else {
+                Fy = this.calcForceExertedBy(allPlanets[i])
+                    *(allPlanets[i].yyPos - this.yyPos)
+                    /this.calcDistance(allPlanets[i]);
+            }
+            sum += Fy;
+        }
+        return sum;
+    }
     
     
 }
