08c16d614ade5a56d8c54aa553a03850a4559d97
RuochenHan <RuochenHan@dhcp-10-142-251-51.uawifi.arizona.edu>
2017-05-05 20:28:21 -0700

    Project 1b complete!

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -12,6 +12,7 @@ public class Palindrome {
     
     // Return true if the given word is palindrome, and false otherwise
     public static boolean isPalindrome(String word) {
+        
         // Generate a deque containing each character in the word
         Deque<Character> deque = wordToDeque(word);
         
@@ -32,7 +33,35 @@ public class Palindrome {
             }
     }
     
+    /* 
+     * Return true if the word is palindrome according to the character comparison test
+     * provided by the CharacterComparator passed in as argument cc
+     */
+    public static boolean isPalindrome(String word, CharacterComparator cc) {
+        
+        // Generate a deque containing each character in the word
+        Deque<Character> deque = wordToDeque(word);
+        
+        // Any word of length 1 or 0 is a palindrome
+        if (word.length() == 0 || word.length() == 1) {
+            return true;
+        }
+        int ct = 0;
+        for (int i = 0; i < word.length()/2; i++) {
+            char f = deque.removeFirst();
+            char l = deque.removeLast();
+            if (cc.equalChars(f, l)) {ct++;}
+        }
+        if (ct == word.length()/2) {
+            return true;
+            } else {
+                return false;
+            }
+        
+    }
+    
     public static void main(String[] args) {
+        // Simple test wordToDeque
         String word = "raceedfcar";
         Deque<Character> deque = wordToDeque(word);
         deque.printDeque();
--- proj1b/PalindromeFinder.java
+++ proj1b/PalindromeFinder.java
@@ -1,6 +1,9 @@
 /** This class outputs all palindromes in the words file in the current directory. */
 public class PalindromeFinder {
     public static void main(String[] args) {
+        
+        // Uncomment to find all the padlindromes equal or longer than 4
+        /*
         int minLength = 4;
         In in = new In("word.txt");
 
@@ -10,5 +13,34 @@ public class PalindromeFinder {
                 System.out.println(word);
             }
         }
+        */
+        
+        // Uncomment to find all the off-by-1 padlindromes equal or longer than 4
+        /*
+        OffByOne offby1 = new OffByOne();
+        int minLength = 4;
+        In in = new In("word.txt");
+
+        while (!in.isEmpty()) {
+            String word = in.readString();
+            if (word.length() >= minLength && Palindrome.isPalindrome(word, offby1)) {
+                System.out.println(word);
+            }
+        }
+        */
+        
+        // Uncomment to find all the off-by-N padlindromes equal or longer than 4. In this case, N = 5
+        /*
+        OffByN offby5 = new OffByN(5);
+        int minLength = 4;
+        In in = new In("word.txt");
+
+        while (!in.isEmpty()) {
+            String word = in.readString();
+            if (word.length() >= minLength && Palindrome.isPalindrome(word, offby5)) {
+                System.out.println(word);
+            }
+        }
+        */
     }
 } 
