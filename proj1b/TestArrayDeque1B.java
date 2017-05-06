db37d741d4d408edfa4148be964b31ff7e750bf8
HanchenXT <ruochenhan@gmail.com>
2017-05-05 23:52:51 -0700

    Project 1b phase I complete!

--- proj1b/TestArrayDeque1B.java
+++ proj1b/TestArrayDeque1B.java
@@ -2,77 +2,127 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestArrayDeque1B {
+  
+    @Test
+    public void testSize() {
+        
+        int add = StdRandom.uniform(50, 100);
+        int remove = StdRandom.uniform(49);
+        OperationSequence fs = new OperationSequence();
+        //StudentArrayDeque<Integer> stu = new StudentArrayDeque<>();
+        //ArrayDeque<Integer> stu = new ArrayDeque<>();
+        LinkedListDeque<Integer> stu = new LinkedListDeque<>();
+        ArrayDequeSolution<Integer> sol = new ArrayDequeSolution<Integer>();
+        for (int i = 0; i < add; i++) {
+            int num = StdRandom.uniform(100);
+            stu.addFirst(num);
+            sol.addFirst(num);
+            DequeOperation dequeOp1 = new DequeOperation("addFirst", num);
+            fs.addOperation(dequeOp1);
+        }
+        
+        for (int i = 0; i <remove; i++) {
+            
+            stu.removeFirst();
+            sol.removeFirst();
+            DequeOperation dequeOp2 = new DequeOperation("removeFirst");
+            fs.addOperation(dequeOp2);
+        }
+        assertEquals(fs.toString(), sol.size(), stu.size());
+    }
     
     @Test
     public void testaddFirst() {
         
-        int num1 = StdRandom.uniform(100);
-        int num2 = StdRandom.uniform(100);
-        
+        int add = 100;
         OperationSequence fs = new OperationSequence();
-        DequeOperation dequeOp1 = new DequeOperation("addFirst", num1);
-        DequeOperation dequeOp2 = new DequeOperation("addLast", num2);
-        DequeOperation dequeOp3 = new DequeOperation("removeFirst");
-        DequeOperation dequeOp4 = new DequeOperation("removeLast");
-
-
-        fs.addOperation(dequeOp1);
-        fs.addOperation(dequeOp2);
-        fs.addOperation(dequeOp3);
-        fs.addOperation(dequeOp4);
-
-        System.out.println(fs.toString());
-
-        StudentArrayDeque<Integer> stu = new StudentArrayDeque<>();
+        //StudentArrayDeque<Integer> stu = new StudentArrayDeque<>();
+        //ArrayDeque<Integer> stu = new ArrayDeque<>();
+        LinkedListDeque<Integer> stu = new LinkedListDeque<>();
         ArrayDequeSolution<Integer> sol = new ArrayDequeSolution<Integer>();
-
-        stu.addFirst(num1);
-        sol.addFirst(num1);
-        assertEquals(fs.toString(), sol, stu);
+        for (int i = 0; i < add; i++) {
+            int num = StdRandom.uniform(100);
+            stu.addFirst(num);
+            sol.addFirst(num);
+            DequeOperation dequeOp1 = new DequeOperation("addFirst", num);
+            fs.addOperation(dequeOp1);
+            assertEquals(fs.toString(), sol.getFirst(),stu.get(0));
+        }
+        
     }
     
     @Test
     public void testaddLast() {
-        StudentArrayDeque<Integer> stu = new StudentArrayDeque<>();
+        
+        int add = 100;
+        OperationSequence fs = new OperationSequence();
+        //StudentArrayDeque<Integer> stu = new StudentArrayDeque<>();
+        //ArrayDeque<Integer> stu = new ArrayDeque<>();
+        LinkedListDeque<Integer> stu = new LinkedListDeque<>();
         ArrayDequeSolution<Integer> sol = new ArrayDequeSolution<Integer>();
-        int num = StdRandom.uniform(100);
-        stu.addLast(num);
-        sol.addLast(num);
-        assertEquals(sol, stu);
+        for (int i = 0; i < add; i++) {
+            int num = StdRandom.uniform(100);
+            stu.addLast(num);
+            sol.addLast(num);
+            DequeOperation dequeOp1 = new DequeOperation("addLast", num);
+            fs.addOperation(dequeOp1);
+            assertEquals(fs.toString(), sol.get(sol.size()-1),stu.get(stu.size()-1));
+        }
+        
     }
     
     @Test
     public void testremoveFirst() {
-        StudentArrayDeque<Integer> stu = new StudentArrayDeque<>();
+        
+        int add = 100;
+        int remove = 100;
+        OperationSequence fs = new OperationSequence();
+        //StudentArrayDeque<Integer> stu = new StudentArrayDeque<>();
+        //ArrayDeque<Integer> stu = new ArrayDeque<>();
+        LinkedListDeque<Integer> stu = new LinkedListDeque<>();
         ArrayDequeSolution<Integer> sol = new ArrayDequeSolution<Integer>();
-        int num1 = StdRandom.uniform(100);
-        int num2 = StdRandom.uniform(100);
-        stu.addFirst(num1);
-        sol.addFirst(num1);
-        stu.addLast(num2);
-        sol.addLast(num2);
-        stu.removeFirst();
-        sol.removeFirst();
-        assertEquals(sol, stu);
+        for (int i = 0; i < add; i++) {
+            int num = StdRandom.uniform(100);
+            stu.addFirst(num);
+            sol.addFirst(num);
+        }
+        for (int j = 0; j < remove; j++) {
+            int st = stu.removeFirst();
+            int so = sol.removeFirst();
+            DequeOperation dequeOp1 = new DequeOperation("removeFirst");
+            fs.addOperation(dequeOp1);
+            assertEquals(fs.toString(), so, st);
+        }
+        
     }
     
     @Test
     public void testremoveLast() {
-        StudentArrayDeque<Integer> stu = new StudentArrayDeque<>();
+        
+        int add = 100;
+        int remove = 100;
+        OperationSequence fs = new OperationSequence();
+        //StudentArrayDeque<Integer> stu = new StudentArrayDeque<>();
+        //ArrayDeque<Integer> stu = new ArrayDeque<>();
+        LinkedListDeque<Integer> stu = new LinkedListDeque<>();
         ArrayDequeSolution<Integer> sol = new ArrayDequeSolution<Integer>();
-        int num1 = StdRandom.uniform(100);
-        int num2 = StdRandom.uniform(100);
-        stu.addFirst(num1);
-        sol.addFirst(num1);
-        stu.addLast(num2);
-        sol.addLast(num2);
-        stu.removeLast();
-        sol.removeLast();
-        assertEquals(sol, stu);
+        for (int i = 0; i < add; i++) {
+            int num = StdRandom.uniform(100);
+            stu.addLast(num);
+            sol.addLast(num);
+        }
+        for (int j = 0; j < remove; j++) {
+            int st = stu.removeLast();
+            int so = sol.removeLast();
+            DequeOperation dequeOp1 = new DequeOperation("removeLast");
+            fs.addOperation(dequeOp1);
+            assertEquals(fs.toString(), so, st);
+        }
+        
     }
-    
     public static void main(String[] args) {
         // TODO Auto-generated method stub
+        jh61b.junit.TestRunner.runTests("all", TestArrayDeque1B.class);
 
     }
 
